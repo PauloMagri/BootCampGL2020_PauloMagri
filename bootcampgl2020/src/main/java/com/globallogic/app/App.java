5799f2b097f478fa7a24ba0a133a84e1548e0dd2
Paulo <titoki.pm@gmail.com>
2020-07-26 22:56:10 -0300

    Sumando y aplicando condicional

--- bootcampgl2020/src/main/java/com/globallogic/app/App.java
+++ bootcampgl2020/src/main/java/com/globallogic/app/App.java
@@ -6,8 +6,16 @@ package com.globallogic.app;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+    	int numero1 = 3;
+    	int numero2 = 10;
+    	int suma = numero1 + numero2;
+    	
+    	if (suma <= 10) {
+    		System.out.println( "La suma de los numeros es : " + suma );
+    	}else {
+    		System.out.println( "La suma de los numeros es Mayor a 10" );
+    	}
+    	
     }
 }
