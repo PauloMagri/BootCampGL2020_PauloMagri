feb27737155130970a758ce8024bab5c7004149b
Paulo <titoki.pm@gmail.com>
2020-07-27 23:42:12 -0300

    Uso de foreach

--- bootcampgl2020/src/main/java/com/globallogic/app/App.java
+++ bootcampgl2020/src/main/java/com/globallogic/app/App.java
@@ -6,8 +6,14 @@ package com.globallogic.app;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+    	
+    	int[] arreglo = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    	
+    	for(Integer num : arreglo) {
+    		System.out.println( "Valor iterado: " + num );
+    	}
+    	
+        
     }
 }
