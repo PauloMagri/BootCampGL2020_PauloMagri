eafabe62a4aab599c19dc01514755c31e7cf5bf1
Paulo <titoki.pm@gmail.com>
2020-07-28 00:10:00 -0300

    Uso de foreach para imprimir un String

--- bootcampgl2020/src/main/java/com/globallogic/app/App.java
+++ bootcampgl2020/src/main/java/com/globallogic/app/App.java
@@ -6,8 +6,14 @@ package com.globallogic.app;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+    	
+    	String cadena = "BootCamp 2020";
+    	
+    	for(Character letra : cadena.toCharArray() ) {
+    		System.out.println( "Letra: " + letra );
+    	}
+    	
+        
     }
 }
