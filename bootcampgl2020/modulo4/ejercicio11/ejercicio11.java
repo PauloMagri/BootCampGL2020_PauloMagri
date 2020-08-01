058209c9850757762601ccf3cdb684ee43f05f6e
Paulo <titoki.pm@gmail.com>
2020-08-01 20:17:27 -0300

    Se agrego el punto once

---
+++ bootcampgl2020/modulo4/ejercicio11/ejercicio11.java
@@ -0,0 +1,16 @@
+package ejercicio11;
+
+public class ejercicio11 {
+	
+public static void main( String[] args ) {
+    	
+    	String cadena = "BootCamp 2020";
+    	
+    	for(Character letra : cadena.toCharArray() ) {
+    		System.out.println( "Letra: " + letra );
+    	}
+    	
+        
+    }
+
+}
