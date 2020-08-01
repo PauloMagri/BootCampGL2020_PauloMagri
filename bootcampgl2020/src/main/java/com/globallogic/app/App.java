d0e8329aa7831f110389779f657139b027b216cb
Paulo <titoki.pm@gmail.com>
2020-08-01 19:45:08 -0300

    Se agrego modulo4 ejercicio3

--- bootcampgl2020/src/main/java/com/globallogic/app/App.java
+++ bootcampgl2020/src/main/java/com/globallogic/app/App.java
@@ -2,13 +2,10 @@ package com.globallogic.app;
 
 public class App 
 {
-<<<<<<< HEAD
-	public static void main( String[] args ) {}
-=======
+
     public static void main( String[] args ) {
     	StudentDetails estudiante = new StudentDetails();
     	
     	estudiante.studentAge(12);
     }
->>>>>>> 3abf7b88ae894dbfa47300122d360aee88e0714b
 }
