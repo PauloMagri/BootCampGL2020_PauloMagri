90d653aac4b68ba0c60138abdf920ad2aa7dcc80
Paulo <titoki.pm@gmail.com>
2020-08-02 15:15:50 -0300

    Modulo ejercicio 5.3

---
+++ bootcampgl2020/modulo4/modulo5ejercicio3/Gato.java
@@ -0,0 +1,28 @@
+package modulo5ejercicio3;
+
+public class Gato {
+	
+	private String nombre;
+    private int patas;
+
+    public Gato() {}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPatas() {
+		return patas;
+	}
+
+	public void setPatas(int patas) {
+		this.patas = patas;
+	}
+    
+    
+
+}
