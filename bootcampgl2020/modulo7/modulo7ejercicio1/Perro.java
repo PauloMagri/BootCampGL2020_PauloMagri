f54eea2b676ac60af4407dd286b7a479a112e08f
Paulo <titoki.pm@gmail.com>
2020-08-09 00:51:14 -0300

    Modulo 7 ejercicios 1, 2, 3

---
+++ bootcampgl2020/modulo7/modulo7ejercicio1/Perro.java
@@ -0,0 +1,15 @@
+package modulo7ejercicio1;
+
+public class Perro implements Animal {
+	
+	public void dormir() {
+		System.out.println("Zzzz");
+	}
+	
+	public void sonidoAnimal() {
+		System.out.println("El perro hace guau guau guau");
+	}
+	
+	
+
+}
---
+++ bootcampgl2020/modulo7/modulo7ejercicio1/package-info.java
@@ -0,0 +1,1 @@
+package modulo7ejercicio1;
